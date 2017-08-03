e4c05f0ee71931ff90b464c2020a2a76c6d4ec8a
Ezequiel de Valais <ezequiel.devalais@gmail.com>
2017-08-03 10:22:42 -0300

    websocket sample

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,8 +1,9 @@
 package controllers;
 
 import play.*;
+import play.libs.F.Callback;
+import play.libs.F.Callback0;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
@@ -24,5 +25,24 @@ public class Application extends Controller {
     public static Result ventas() {
         Content html = ventas.render();
         return ok(html);
+    }
+    
+    public static WebSocket<String> hello() {
+        return new WebSocket<String>() {
+            public void onReady(WebSocket.In<String> in,
+                    WebSocket.Out<String> out) {
+                in.onMessage(new Callback<String>() {
+                    public void invoke(String event) {
+                        System.out.println(event);
+                    }
+                });
+                in.onClose(new Callback0() {
+                    public void invoke() {
+                        System.out.println("Disconnected");
+                    }
+                });
+                out.write("Hello client!");
+            }
+        };
     }
 }
